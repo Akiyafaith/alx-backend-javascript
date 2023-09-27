4f43d70feda7b064bd40736dac8d51ff19c9fa9e
akiyafaith <akiyafaith@gmail.com>
2023-09-27 11:43:21 +0000

    promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -2,10 +2,12 @@ function getResponseFromAPI() {
   return new Promise((resolve) => {
     setTimeout(() => {
       const response = {
-	status: 200,
+        status: 200,
         body: 'photo-profile-1',
       };
       resolve(response);
     }, 1000);
-   });
+  });
 }
+
+export default getResponseFromAPI;
