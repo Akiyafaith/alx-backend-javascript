96c77bad165a791261ec0a5f6f2e5b1376aa7b5f
Faith Nyaboke <akiyafaith@gmail.com>
2023-10-22 14:05:43 +0300

    classes

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,17 +1,19 @@
-function Building(sqft) {
-  if (this.constructor === Building) {
-    throw new TypeError('Cannot instantiate abstract class.');
+class Building {
+  constructor(sqft) {
+    if (this.constructor === Building) {
+      throw new TypeError('Cannot instantiate abstract class.');
+    }
+
+    this._sqft = sqft;
+
+    if (typeof this.evacuationWarningMessage !== 'function') {
+      throw new TypeError('Class extending Building must override evacuationWarningMessage');
+    }
   }
 
-  this._sqft = sqft;
-
-  if (typeof this.evacuationWarningMessage !== 'function') {
-    throw new TypeError('Class extending Building must override evacuationWarningMessage');
+  get_sqft() {
+    return this._sqft;
   }
 }
-
-Building.prototype.get_sqft = function () {
-  return this._sqft;
-};
 
 export default Building;
