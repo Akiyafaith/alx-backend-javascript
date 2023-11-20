f442615e4d889ade9221c94a20ba11813802ac6c
Faith Nyaboke <akiyafaith@gmail.com>
2023-11-20 22:44:04 +0300

    NodeJS

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,19 +1,23 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+function interactiveSession() {
+  process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.setEncoding('utf8');
+  process.stdin.setEncoding('utf8');
 
-process.stdin.on('data', (data) => {
-  const name = data.trim();
-  if (name) {
-    process.stdout.write(`Your name is: ${name}\n`);
-  } else {
-    process.stdout.write('Your name is: \n');
-  }
+  process.stdin.on('data', (data) => {
+    const name = data.trim();
+    if (name) {
+      process.stdout.write(`Your name is: ${name}\n`);
+    } else {
+      process.stdout.write('Your name is: \n');
+    }
 
-  process.stdout.write('This important software is now closing\n');
-  process.exit();
-});
+    process.stdout.write('This important software is now closing\n');
+    process.exit();
+  });
 
-process.on('exit', () => {
-  process.stdout.write('\n');
-});
+  process.on('exit', () => {
+    process.stdout.write('\n');
+  });
+}
+
+module.exports = interactiveSession;
