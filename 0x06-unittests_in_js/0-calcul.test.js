77fc3f72b8071180007d7b7043ed6c3634363aaf
Faith Nyaboke <akiyafaith@gmail.com>
2023-11-24 01:41:20 +0300

    unittest

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,8 +1,8 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', function () {
-  it('should return the rounded sum of two numbers', function () {
+describe('calculateNumber', () => {
+  it('should return the rounded sum of two numbers', () => {
     assert.strictEqual(calculateNumber(1, 3), 4);
     assert.strictEqual(calculateNumber(1, 3.7), 5);
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
