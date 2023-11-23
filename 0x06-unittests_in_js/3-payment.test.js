3ff6f4c7d3daba8a2e619acb23920152eca842f4
Faith Nyaboke <akiyafaith@gmail.com>
2023-11-24 02:22:10 +0300

    payment

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,23 +1,13 @@
 const sinon = require('sinon');
-const assert = require('assert');
+const { expect } = require('chai');
 const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', () => {
-  it('should call Utils.calculateNumber with correct arguments and log the result', () => {
+  it('should use Utils.calculateNumber with type SUM', () => {
     const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
-    const consoleLogSpy = sinon.spy(console, 'log');
-
     sendPaymentRequestToApi(100, 20);
-
-    // Assert that Utils.calculateNumber was called with the correct arguments
-    assert(calculateNumberSpy.calledWith('SUM', 100, 20));
-
-    // Assert that the console.log was called with the correct message
-    assert(consoleLogSpy.calledWith('The total is: 120'));
-
-    // Restore the spies to their original state
+    expect(calculateNumberSpy.calledWithExactly('SUM', 100, 20)).to.be.true;
     calculateNumberSpy.restore();
-    consoleLogSpy.restore();
   });
 });
